d8ecb3864500e22d9db11d8a5db7c2dac578f4c9
Khaled 01 <57640596+5aled01@users.noreply.github.com>
2021-05-06 06:14:19 -1200

    Test commit 1

---
+++ src/main/java/com/iscae/GetionLocation/GetionLocationApplication.java
@@ -0,0 +1,13 @@
+package com.iscae.GetionLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetionLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetionLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iscae/GetionLocation/GetionLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iscae.GetionLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetionLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
