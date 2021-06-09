b46b07c51bda1cecfc937009fcd4ca893949bb06
Khaled 01 <57640596+5aled01@users.noreply.github.com>
2021-06-09 12:11:52 +0000

    09/06/2021

--- src/main/java/com/iscae/GetionLocation/model/ProC1.java
+++ src/main/java/com/iscae/GetionLocation/model/ProC1.java
@@ -2,7 +2,7 @@ package com.iscae.GetionLocation.model;
 import javax.persistence.*;
 import java.io.Serializable;
 @Entity
-public class ProC1 extends Poprietaire implements Serializable {
+public class ProC1 extends Poprietaire {
 
     private String Nom;
     private String Prenom;
@@ -10,11 +10,13 @@ public class ProC1 extends Poprietaire implements Serializable {
     private Long Numcompo;
 
     public ProC1(Long id, Long telephone, String adresse, String nom, String prenom, Long NNI, Long numcompo) {
+
         super(id, telephone, adresse);
         this.Nom = nom;
         this.Prenom = prenom;
         this.NNI = NNI;
         this.Numcompo = numcompo;
+
     }
 
     public String getNom() {
